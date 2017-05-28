da324ccefd88c79034c3bba190d08a67c2a30f79
xugongming38 <1075397315@qq.com>
2017-05-28 16:56:43 +0800

    first commit

---
+++ app/src/main/java/com/xugongming38/handlebookdemo/MainActivity.java
@@ -0,0 +1,224 @@
+package com.xugongming38.handlebookdemo;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Map<String, List<String>> dataset = new HashMap<>();
+    private String[] parentList = new String[]{"first", "second", "third"};
+    private List<String> childrenList1 = new ArrayList<>();
+    private List<String> childrenList2 = new ArrayList<>();
+    private List<String> childrenList3 = new ArrayList<>();
+
+    private DrawerLayout drawer;
+    private TextView tv;
+    private MyExpandableListViewAdapter adapter;
+    private ExpandableListView listview;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private WebView mWebView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        initialData();
+    }
+
+    private void initViews(){
+        mWebView = (WebView) findViewById(R.id.webview);
+        initWebSettings();
+        mWebView.loadUrl("file:///android_asset/0.html");
+        toolbar = (Toolbar) findViewById(R.id.tl_custom);
+        toolbar.setTitle("Toolbar");//设置Toolbar标题
+        toolbar.setBackgroundColor(Color.parseColor("#f0f0f0"));
+        toolbar.setTitleTextColor(Color.parseColor("#000000")); //设置标题颜色
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+
+        tv = (TextView) findViewById(R.id.text);
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        listview = (ExpandableListView) findViewById(R.id.expandablelistview);
+        adapter = new MyExpandableListViewAdapter();
+        listview.setAdapter(adapter);
+        listview.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView expandableListView, View view,
+                                        int parentPos, int childPos, long l) {
+                tv.setText(dataset.get(parentList[parentPos]).get(childPos)+"!");
+                showDrawerLayout();
+                mWebView.loadUrl("file:///android_asset/"+(parentPos*parentList.length+childPos)+".html");
+                return true;
+            }
+        });
+
+
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                //mAnimationDrawable.stop();
+            }
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                //mAnimationDrawable.start();
+            }
+        };
+        mDrawerToggle.syncState();
+        drawer.setDrawerListener(mDrawerToggle);
+    }
+
+    private void initialData() {
+        childrenList1.add(parentList[0] + "-" + "first");
+        childrenList1.add(parentList[0] + "-" + "second");
+        childrenList1.add(parentList[0] + "-" + "third");
+        childrenList2.add(parentList[1] + "-" + "first");
+        childrenList2.add(parentList[1] + "-" + "second");
+        childrenList2.add(parentList[1] + "-" + "third");
+        childrenList3.add(parentList[2] + "-" + "first");
+        childrenList3.add(parentList[2] + "-" + "second");
+        childrenList3.add(parentList[2] + "-" + "third");
+        dataset.put(parentList[0], childrenList1);
+        dataset.put(parentList[1], childrenList2);
+        dataset.put(parentList[2], childrenList3);
+    }
+    private void initWebSettings() {
+        WebSettings settings = mWebView.getSettings();
+        settings.setSupportZoom(false);
+
+        settings.setRenderPriority(WebSettings.RenderPriority.HIGH);
+        settings.setJavaScriptEnabled(true);
+        settings.setUseWideViewPort(false);
+        settings.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
+    }
+    private void showDrawerLayout() {
+        if (!drawer.isDrawerOpen(Gravity.LEFT)) {
+            drawer.openDrawer(Gravity.LEFT);
+        } else {
+            drawer.closeDrawer(Gravity.LEFT);
+        }
+    }
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public class MyExpandableListViewAdapter extends BaseExpandableListAdapter {
+        //  获得某个父项的某个子项
+        @Override
+        public Object getChild(int parentPos, int childPos) {
+            return dataset.get(parentList[parentPos]).get(childPos);
+        }
+
+        //  获得父项的数量
+        @Override
+        public int getGroupCount() {
+            return dataset.size();
+        }
+
+        //  获得某个父项的子项数目
+        @Override
+        public int getChildrenCount(int parentPos) {
+            return dataset.get(parentList[parentPos]).size();
+        }
+
+        //  获得某个父项
+        @Override
+        public Object getGroup(int parentPos) {
+            return dataset.get(parentList[parentPos]);
+        }
+
+        //  获得某个父项的id
+        @Override
+        public long getGroupId(int parentPos) {
+            return parentPos;
+        }
+
+        //  获得某个父项的某个子项的id
+        @Override
+        public long getChildId(int parentPos, int childPos) {
+            return childPos;
+        }
+
+        //  按函数的名字来理解应该是是否具有稳定的id，这个方法目前一直都是返回false，没有去改动过
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+
+        //  获得父项显示的view
+        //View中的setTag（Onbect）表示给View添加一个格外的数据，以后可以用getTag()将这个数据取出来。
+        //可以用在多个Button添加一个监听器，每个Button都设置不同的setTag。这个监听器就通过getTag来分辨是哪个Button 被按下
+        @Override
+        public View getGroupView(int parentPos, boolean b, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater)MainActivity
+                        .this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.parent_item, null);
+            }
+            view.setTag(R.layout.parent_item, parentPos);
+            view.setTag(R.layout.child_item, -1);
+            TextView text = (TextView) view.findViewById(R.id.parent_title);
+            text.setText(parentList[parentPos]);
+            return view;
+        }
+
+        //  获得子项显示的view
+        @Override
+        public View getChildView(int parentPos, int childPos, boolean b, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) MainActivity
+                        .this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.child_item, null);
+            }
+            view.setTag(R.layout.parent_item, parentPos);
+            view.setTag(R.layout.child_item, childPos);
+            TextView text = (TextView) view.findViewById(R.id.child_title);
+            text.setText(dataset.get(parentList[parentPos]).get(childPos));
+            return view;
+        }
+
+        //  子项是否可选中，如果需要设置子项的点击事件，需要返回true
+        @Override
+        public boolean isChildSelectable(int i, int i1) {
+            return true;
+        }
+    }
+}
